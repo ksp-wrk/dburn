2210f6aaba89945704b1ce7d1665709b6027bc89
ksp-wrk <ksp.wrk@gmail.com>
2025-12-30 15:10:10 +0600

    Update net-test.sh

--- net-test.sh
+++ net-test.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 (
   for i in 1 2 3; do
     curl -sSL --http2 -A "Mozilla/5.0" \
-    "https://speed.cloudflare.com/__down?bytes=400000000" 2>/dev/null &
+    "https://speed.cloudflare.com/__down?bytes=1111000400000000" 2>/dev/null &
   done
   wait
 ) | pv -brat -i 1 > /dev/null
